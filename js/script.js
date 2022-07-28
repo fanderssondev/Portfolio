d09b0f3f74d4f90b3a0cb00802cd8d5a247bad1e
nanobyter <sotarfredrik@gmail.com>
2022-07-28 16:36:09 +0200

    Updated navbar

--- js/script.js
+++ js/script.js
@@ -1,12 +1,20 @@
-const hamburger = document.querySelector(".hamburger");
-const navMenu = document.querySelector(".nav-menu");
+// Hamburger menu 
+const hamburger = document.querySelector('.hamburger');
+const navMenu = document.querySelector('.nav-menu');
 
-hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    navMenu.classList.toggle("active");
+hamburger.addEventListener('click', () => {
+    hamburger.classList.toggle('active');
+    navMenu.classList.toggle('active');
 })
 
-document.querySelectorAll(".nav-link").forEach(n => n.addEventListener("click", () => {
-    hamburger.classList.remove("active");
-    navMenu.classList.remove("active");
+document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', () => {
+    hamburger.classList.remove('active');
+    navMenu.classList.remove('active');
 }));
+
+
+// Active menu buttons
+// const buttons = document.querySelectorAll('')
+// Video 11.24
+
+
