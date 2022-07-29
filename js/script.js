8d7e6cc8cb03965c6b156e51983509a2b0e6525f
nanobyter <sotarfredrik@gmail.com>
2022-07-29 16:03:28 +0200

    Active navbar buttons removed

--- js/script.js
+++ js/script.js
@@ -11,29 +11,3 @@ document.querySelectorAll('.nav-link').forEach(n => n.addEventListener('click', 
     hamburger.classList.remove('active');
     navMenu.classList.remove('active');
 }));
-
-
-// Active menu buttons
-const sections = document.querySelectorAll('section');
-const navList = document.querySelectorAll('nav .navbar ul li');
-
-window.addEventListener('scroll', () => {
-    let current = '';
-
-    sections.forEach(section => {
-        const sectionTop = section.offsetTop;
-        const sectionHeight = section.clientHeight;
-
-        if (scrollY >= sectionTop) {
-            current = section.getAttribute('id');
-            console.log(current);
-        }
-    })
-})
-
-
-
-
-
-/////////////////////////////////////////
-//Scroll
